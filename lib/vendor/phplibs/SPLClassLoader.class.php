af0204febebbf22c65d961d64c1fa35b959ef4c6
sque <sque@bogo>
2010-05-06 18:23:30 +0300

    [Licence] Add licence tag in all source files as GPLv3 [Skeleton] Add forgotten file in sources.

--- lib/vendor/phplibs/SPLClassLoader.class.php
+++ lib/vendor/phplibs/SPLClassLoader.class.php
@@ -1,22 +1,25 @@
 <?php
-
 /**
- * SplClassLoader implementation that implements the technical interoperability
- * standards for PHP 5.3 namespaces and class names.
- *
- * http://groups.google.com/group/php-standards/web/final-proposal
- *
- * // Example which loads classes for the Doctrine Common package in the
- * // Doctrine\Common namespace.
- * $classLoader = new SplClassLoader('Doctrine\Common', '/path/to/doctrine');
- * $classLoader->register();
- *
- * @author Jonathan H. Wage <jonwage@gmail.com>
- * @author Roman S. Borschel <roman@code-factory.org>
- * @author Matthew Weier O'Phinney <matthew@zend.com>
- * @author Kris Wallsmith <kris.wallsmith@gmail.com>
- * @author Fabien Potencier <fabien.potencier@symfony-project.org>
+ *  This file is part of PHPLibs <http://phplibs.kmfa.net/>.
+ *  
+ *  Copyright (c) 2010 < squarious at gmail dot com > .
+ *  
+ *  PHPLibs is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  PHPLibs is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with PHPLibs.  If not, see <http://www.gnu.org/licenses/>.
+ *  
  */
+
+
 class SplClassLoader
 {
     private $_fileExtension = '.php';
